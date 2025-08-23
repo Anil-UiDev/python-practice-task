c29e8b75093f4d42f41a0521db0105855a09e6c1
Anil Kumar <56149175+Anil-UiDev@users.noreply.github.com>
2025-08-23 12:05:33 +0530

    Create task2.py

---
+++ task2.py
@@ -0,0 +1,6 @@
+first_name = input("Enter Your First Name : ")
+last_name = input("Enter Your Last Name : ")
+
+full_name = first_name+" " + last_name
+
+print(f"Hello, {full_name}! Welcome to the Python program.")
